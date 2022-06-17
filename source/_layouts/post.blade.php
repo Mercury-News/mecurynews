4f0dc4b8b4afe45b162f2ffbd4933fe771e6d690
Max H <maxplaysbest@gmail.com>
2022-06-17 13:32:20 +1200

    Update post.blade.php

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -36,5 +36,6 @@
     @else
         <p>Comments are not enabled for this post.</p>
     @endif
-    <img src='https://res.cloudinary.com/maxsamazingsite/image/upload/v1655429058/Mercury%20News/Proud_Sponsor_psmjiv.png' alt='Proud Sponsor Of Beam Prime Records'>
+    <a href="https://records.beamprime.tk"><img src='https://res.cloudinary.com/maxsamazingsite/image/upload/v1655429058/Mercury%20News/Proud_Sponsor_psmjiv.png' alt='Proud Sponsor Of Beam Prime Records'>
+    </a>
 @endsection
