2f8c8eb85888d86cfd54c087b2dd6adf9fbe04b7
Max H <maxplaysbest@gmail.com>
2022-06-17 13:52:30 +1200

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -8,6 +8,9 @@
     <h2>Welcome to Mercury News</h2>
     <p>Mercury News Is An Open Source News Service where anyone can contribute.</p>
     <a href="https://mercurynews.tk/posts/how-to-write-articles/"><p>To Write A Article Visit This Page</p></a>
+    <p>Hope You Enjoy!</p>
+    <small>Created By Mercury Team</small>
+    <small>Mercury Is Not Responsible For Any Articles On This Website</small>
 
     <h2>Links:</h2>
 
