3d16d933ee8624f5d896f35a4d1efbde6e5ff701
Max H <maxplaysbest@gmail.com>
2022-06-17 13:54:38 +1200

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -8,4 +8,6 @@
     <a href="https://mercurynews.netlify.app/posts/how-to-write-articles/" target="_blank" rel="noopener">
         <p>To Submit Go Here</p>
     </a>
+
+    <a href='https://records.beamprime.tk'><img src="https://res.cloudinary.com/maxsamazingsite/image/upload/v1655429058/Mercury%20News/Proud_Sponsor_psmjiv.png" alt='Proud Sponsor Of Beam Prime Records'></a>
 @endsection
