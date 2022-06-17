a628ed83975a07de1725e557decec76bfb382b5c
Max H <maxplaysbest@gmail.com>
2022-06-17 16:39:34 +1200

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -9,7 +9,7 @@
         <p>To Submit Go Here</p>
     </a>
 
-    <img src="https://res.cloudinary.com/maxsamazingsite/image/upload/v1655439542/Mercury%20News/logo_size_rcpngp.jpg" alt="Mercury News Logo">
+    <img src="https://res.cloudinary.com/maxsamazingsite/image/upload/v1655439542/Mercury%20News/logo_size_rcpngp.jpg" alt="Mercury News Logo" class="center">
 
     <a href='https://records.beamprime.tk'><img src="https://res.cloudinary.com/maxsamazingsite/image/upload/v1655429058/Mercury%20News/Proud_Sponsor_psmjiv.png" alt='Proud Sponsor Of Beam Prime Records'></a>
 @endsection
